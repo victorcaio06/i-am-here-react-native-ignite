7259fb29d017f1ad7b46e9bbc2965c56a01e5204
Victor Caio <kaioaraujo426@gmail.com>
2023-06-21 13:43:13 -0300

    FEAT: added text input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 import { styles } from './styles';
 
@@ -8,6 +8,12 @@ export default function Home() {
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2023</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     backgroundColor: '#131016',
   },
   eventName: {
-    color: '#FDFCFE',
+    color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48,
@@ -16,5 +16,12 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  input: {
+    height: 56,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16,
+    borderRadius: 5,
+    backgroundColor: '#1FE25',
+  },
 });
-
