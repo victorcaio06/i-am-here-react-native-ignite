ccc7f2454c9c84bd66d22e8a9851378880699920
Victor Caio <kaioaraujo426@gmail.com>
2023-06-22 08:29:29 -0300

    FEAT: added text input and button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,29 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log('Clicou no botão de adicionar');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2023</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,22 +6,48 @@ export const styles = StyleSheet.create({
     padding: 24,
     backgroundColor: '#131016',
   },
+
   eventName: {
     color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48,
   },
+
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16,
   },
+
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  },
+
   input: {
+    flex: 1,
     height: 56,
-    color: '#FFF',
     padding: 16,
     fontSize: 16,
+    color: '#FFF',
+    marginRight: 8,
     borderRadius: 5,
-    backgroundColor: '#1FE25',
+    backgroundColor: '#1F1E25',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#31CF67',
   },
 });
